cd53be65661dc5c460b0720761a776264e036b3a
HurubeanAlexia <91553380+HurubeanAlexia@users.noreply.github.com>
2024-01-11 14:29:06 +0200

    pagina informatii

---
+++ gym-buddy-frontend/src/pagina-informatii/pagina-informatii/modal.js
@@ -0,0 +1,55 @@
+const quote1 = document.getElementById('quote1');
+const modal_container1 = document.getElementById('modal-container1');
+const close1 = document.getElementById('close1');
+
+quote1.addEventListener('click', () => {
+    modal_container1.classList.add('show');
+});
+
+close1.addEventListener('click', () => {
+    modal_container1.classList.remove('show');
+});
+
+
+//alimentatie
+
+const quote2 = document.getElementById('quote2');
+const modal_container2 = document.getElementById('modal-container2');
+const close2 = document.getElementById('close2');
+
+quote2.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+});
+
+close2.addEventListener('click', () => {
+    modal_container2.classList.remove('show');
+});
+
+//muschi
+
+const quote3 = document.getElementById('quote3');
+const modal_container3 = document.getElementById('modal-container3');
+const close3 = document.getElementById('close3');
+
+quote3.addEventListener('click', () => {
+    modal_container3.classList.add('show');
+});
+
+close3.addEventListener('click', () => {
+    modal_container3.classList.remove('show');
+});
+
+
+//suplimente
+
+const quote4 = document.getElementById('quote4');
+const modal_container4 = document.getElementById('modal-container4');
+const close4 = document.getElementById('close4');
+
+quote4.addEventListener('click', () => {
+    modal_container4.classList.add('show');
+});
+
+close4.addEventListener('click', () => {
+    modal_container4.classList.remove('show');
+});
